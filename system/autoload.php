0883193af3c76260dcbdcdac303372fa5e194bbf
Mark <markusx64bit@inbox.ru>
2020-04-02 14:52:35 +0800

    fix autoload

--- system/autoload.php
+++ system/autoload.php
@@ -4,6 +4,7 @@ spl_autoload_register(function ($class) {
     if (trim($class, '\\') == 'Sys') {
         include __DIR__ . '/Sys.php';
     } else {
-        include __DIR__ . '/../' . $class . '.php';
+		$pathClass = str_replace('\\', '/', $class);
+        include __DIR__ . '/../' . $pathClass . '.php';
     }
 });
